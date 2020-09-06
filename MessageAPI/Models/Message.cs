9c942345f99f5121ae7a309692ce3dc2bb2cf137
tmlima <thiago.machado.lima>
2020-09-06 16:18:47 -0300

    initial commit

---
+++ MessageAPI/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageAPI.Model
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ MessageAPI/Models/MessageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MessageAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessageAPI.Models
+{
+    public class MessageContext : DbContext
+    {
+        public MessageContext(DbContextOptions<MessageContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}

066bfa620886b77a2d12f6205eb189cea9774fa6
tmlima <thiago.machado.lima>
2020-09-06 16:35:28 -0300

    adding dbcontext to startup

--- MessageAPI/Models/MessageContext.cs
+++ MessageAPI/Models/MessageContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MessageAPI.Model;
+﻿using MessageAPI.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace MessageAPI.Models
--- MessageAPI/Startup.cs
+++ MessageAPI/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MessageAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MessageAPI
 {
@@ -22,13 +17,22 @@ namespace MessageAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var server = Configuration["DBServer"] ?? "localhost";
+            var port = Configuration["DBPort"] ?? "1433";
+            var databaseName = "message";
+
+            // TODO remove credentials from here
+            var user = Configuration["DBUser"] ?? "SA";
+            var password = Configuration["DBPassword"] ?? "Pa$$word2020";
+
+            services.AddDbContext<MessageContext>(options =>
+                options.UseSqlServer($"Server={server},{port};Initial Catalog={databaseName};User ID={user};Password={password}");
+
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
